e7dbbaecd449395aaf3858abf5047ae2250a7362
Anthony Viera <anthonyviera4@gmail.com>
2026-07-10 08:37:46 -0500

    Initial Commit

---
+++ smart_water/smart_waterApp.swift
@@ -0,0 +1,17 @@
+//
+//  smart_waterApp.swift
+//  smart_water
+//
+//  Created by Anthony Viera on 7/10/26.
+//
+
+import SwiftUI
+
+@main
+struct smart_waterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
